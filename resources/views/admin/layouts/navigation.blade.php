dbf46e5c2a8b109aa21e571280da837a77b95800
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-05-25 10:15:59 -0400

    Changing posittion for employees link

--- resources/views/admin/layouts/navigation.blade.php
+++ resources/views/admin/layouts/navigation.blade.php
@@ -7,7 +7,10 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="{{ route('admin.index') }}">{{ __('Dashboard') }}</a>
+            <a class="nav-link active" href="{{ route('admin.index') }}">{{ __('Dashboard') }}</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="{{ route('admin.employee.index') }}">{{ __('Employees') }}</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -17,7 +20,6 @@
               <li><a class="dropdown-item" href="{{ route('admin.classification.index') }}">{{ __('Classifications') }}</a></li>
               <li><a class="dropdown-item" href="{{ route('admin.genre.index') }}">{{ __('Genres') }}</a></li>
               <li><a class="dropdown-item" href="{{ route('admin.role.index') }}">{{ __('Roles') }}</a></li>
-              <li><a class="dropdown-item" href="{{ route('admin.employee.index') }}">{{ __('Employees') }}</a></li>
             </ul>
           </li>
         </ul>
